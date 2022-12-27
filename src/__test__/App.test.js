69cac3cc1bbd94c0de0238ff93c06fc080d6385b
Quartz <skyu.dev@gmail.com>
2022-12-27 17:52:30 +0900

    Style: prettier formatting

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,12 +1,12 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
 
-describe("App 컴포넌트 렌더링 테스트", () => {
-  test("<App /> 렌더링이 되나요?", async () => {
+describe('App 컴포넌트 렌더링 테스트', () => {
+  test('<App /> 렌더링이 되나요?', async () => {
     render(<App />);
 
-    const headingEl = screen.getByRole("heading", makeOptions(1, "app"));
+    const headingEl = screen.getByRole('heading', makeOptions(1, 'app'));
     expect(headingEl).toBeInTheDocument();
   });
 });
